3830d140b06874563e1d68f822c87fdc92e72d79
Duc Vu <duc.vu@quant-edge.com>
2022-07-30 09:50:53 +0700

    update api upload chuck

--- src/file/upload-chunks.js
+++ src/file/upload-chunks.js
@@ -1,8 +1,22 @@
 import fs from 'fs';
 import errorCode from '../../common/error-code.js';
+import { ROLE_NAME } from '../../common/enum.js';
+import { isHasRole } from '../../helper/utils.js';
+import { getUserNameAndFullNameInToken } from '../../user/index.js';
 
-export default function (req, res) {
+export default async function (req, res) {
     try {
+        const { usernameInToken, fullName } = getUserNameAndFullNameInToken(req) || {};
+        req.usernameInToken = usernameInToken;
+        req.fullName = fullName;
+
+        if (!(await isHasRole(usernameInToken, ROLE_NAME.UPLOAD_FILE))) {
+            return res.send({
+                ErrorCode: errorCode.PERMISSION_DENIED,
+                Success: false,
+            });
+        }
+
         const { fileName } = req.query;
         if (!fileName) {
             const msg = `missing fileName query`;
